78883e773717cda6a9d4848c9cf0db6c7e5761d6
Diego Arroyo <diego.arroyo2462050@gmail.com>
2024-01-02 18:46:18 -0300

    ...

--- PractialApps/Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
+++ PractialApps/Northwind.Common.DataContext.Sqlite/NorthwindContextExtensions.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore; // UseSqlite
 using Microsoft.Extensions.DependencyInjection; // IServiceCollection
 namespace Packt.Shared;
+
+/*
+ * Esto se llama extesion method y lo que hace es agregar una funcion o metodo al objeto IServiceCollection
+ * de tal forma que cuando se haga referencia a esta libreria el metodo va a ser accesible desde cualquier
+ * objeto IServiceCollection, en este caso este metodo va ser invocado desde el programa Northwind.Web
+ * TODO ESTO ESTA DIPONIBLE EN PAG 548 LIBRO CHSHARP11 .NET7 - MARCK PRICE
+ */
 public static class NorthwindContextExtensions
 {
     /// <summary>
@@ -14,6 +21,10 @@ public static class NorthwindContextExtensions
     //</returns>
     public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string relativePath = "..")
     {
+        /*
+         * Sospecho yo que todo esto pudiese hacerse en el archivo program.cs del programa (Northwind.Web)
+         * que este utilizando esta libreria
+         */
         string databasePath = Path.Combine(relativePath, "Northwind.db");
         services.AddDbContext<NorthwindContext>(options =>
         {
